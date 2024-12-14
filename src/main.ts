d03b83c7252f7bc2c21ffeb0b8490742c01b235d
Shehzain Hassan <shehzainhassan@gmail.com>
2024-12-14 19:02:09 +0500

    Fixed cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: 'https://furniro-front-end.vercel.app',
     methods: 'GET,POST, DELETE, PUT, PATCH',
     credentials: true,
   });
