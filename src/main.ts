92dae09f4cb44ded367e45c17d28798aced9622c
ShehzainHassan <shehzainhassan@gmail.com>
2024-12-16 11:54:04 +0500

    Added localhost:3000in orgin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://furniro-front-end.vercel.app',
+    origin: ['https://furniro-front-end.vercel.app', 'http://localhost:3000'],
     methods: 'GET,POST, DELETE, PUT, PATCH',
     credentials: true,
   });
